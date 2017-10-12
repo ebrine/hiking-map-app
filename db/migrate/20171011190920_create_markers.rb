a6687925f87d9ab0784ca36c411bf6d2a4943291
Eliza Brine <email": null>
2017-10-11 19:44:05 -0700

    Creates marker model and migration with validations and method for returning positiion

---
+++ db/migrate/20171011190920_create_markers.rb
@@ -0,0 +1,15 @@
+class CreateMarkers < ActiveRecord::Migration
+  def change
+    create_table :markers do |t|
+      t.string :name, null: false
+      t.string :entry_type, null: false
+      t.string :details
+      t.string :image
+      t.integer :lat, null: false
+      t.integer :long, null: false
+      t.belongs_to :user
+
+      t.timestamps null: false
+    end
+  end
+end
