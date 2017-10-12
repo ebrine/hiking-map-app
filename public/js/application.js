983dd7ae59b66a120466a157bab515e06605cfcd
Eliza Brine <email": null>
2017-10-11 17:13:25 -0700

    Gets basic map to appear on index route with two markers

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,17 @@
-$(document).ready(() => {
-
-});
+function initMap() {
+  const uluru = {lat: -25.363, lng: 131.044};
+  const map = new google.maps.Map($('#map')[0], {
+    zoom: 4,
+    center: uluru
+  });
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+  const other = {lat: -25.363, lng: 140.044};
+  const marker2 = new google.maps.Marker({
+    position: other,
+    map: map,
+    title: "other",
+  });
+}
