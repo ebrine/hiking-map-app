5973771b996a6d848ed2d1649e435159260c5fff
Eliza Brine <email": null>
2017-10-11 19:08:02 -0700

    Adds user model and migration with bcrypt password and presence/uniqueness validations. all works

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+require "bcrypt"
+class User < ActiveRecord::Base
+  include BCrypt
+
+  validates :username, :email, :password_hash, presence: true
+  validates :username, :email, uniqueness: true
+
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+end
