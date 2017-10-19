fc11bb8bbc25facc32e34215578551c325f9ef92
Ashishjayandar & Gordon Baker <pair+ashishjayandar.gorb88@devbootcamp.com>
2017-10-18 18:49:56 -0700

    Styling

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,22 @@
 get "/users/new" do
   erb :"/user/registration", layout: false
 end
+
+post '/users' do
+  new_user = User.new(params[:user])
+  if request.xhr?
+    puts new_user
+    if new_user.save
+      'redirect'
+    else
+      content_type :'application/json'
+      new_user.errors.full_messages.to_json
+    end
+  else
+    if new_user.save
+      redirect :'/home'
+    else
+      errors = new_user.errors.full_messages
+    end
+  end
+end
--- public/js/clickers.js
+++ public/js/clickers.js
@@ -2,16 +2,48 @@ $(document).ready(() => {
   $('#login').click((event) => {
     event.preventDefault();
     const url = $(event.currentTarget).attr("href");
-    console.log(url);
     $.ajax({
       url,
     }).done((response) => {
-      console.log(response);
       $("#login").hide();
       $("#register").hide();
       $(".landingnav").append(response);
     });
   });
+
+  $('.landingnav').on("submit", "#login-form",(e) => {
+    console.log("caught");
+    e.preventDefault();
+    const url = $(e.currentTarget).attr('action');
+    const data = $(e.currentTarget).serialize();
+    $.ajax({
+      method: "POST",
+      url,
+      data,
+    }).done((response) => {
+      console.log(response);
+    });
+  });
+
+  $('.landingnav').on("submit", "#registration-form", (e) => {
+    console.log("caught");
+    e.preventDefault();
+    const url = $(e.currentTarget).attr('action');
+    const data = $(e.currentTarget).serialize();
+    $.ajax({
+      method: "POST",
+      url,
+      data,
+    }).done((response) => {
+      if (response === 'redirect') {
+        window.location.href = '/home';
+      }
+      else {
+        
+      }
+    });
+  });
+
   $('#register').click((event) => {
     event.preventDefault();
     const url = $(event.currentTarget).attr("href");
